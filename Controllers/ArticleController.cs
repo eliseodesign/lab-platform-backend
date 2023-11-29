f20b9a5152805f3f95a237f327845a8325509c58
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-11-28 20:33:49 -0600

    chore: :sparkles: standar response provider

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -2,6 +2,7 @@
 using LabPlatform.Models;
 using LabPlatform.Models.DTOs;
 using LabPlatform.Schemas;
+using LabPlatform.Services.Statics;
 using Microsoft.AspNetCore.Mvc;
 using Nelibur.ObjectMapper;
 
@@ -11,67 +12,75 @@ namespace LabPlatform;
 [Route("api/[controller]")]
 public class ArticleController : ControllerBase
 {
- private readonly IArticleService _ArticleService;
+    private readonly IArticleService _ArticleService;
 
-        public ArticleController(IArticleService ArticleService)
+    public ArticleController(IArticleService ArticleService)
+    {
+        _ArticleService = ArticleService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        List<ArticleDTO> articles = await _ArticleService.GetAll();
+        return Ok(Res.Provider(articles, "Articulos obtenidos", true));
+    }
+
+    [HttpGet]
+    [Route("all/{type}")]
+
+    public async Task<IActionResult> GetNotices(string type)
+    {
+        List<ArticleDTO> articles = await _ArticleService.GetAll();
+        var filter = articles.Where(article => article.ArticleType == type).ToList();
+
+        var min = filter.Select(article => TinyMapper.Map<ArticleMinDTO>(article)).ToList();
+        return Ok(Res.Provider(min, "Articulos obtenidos", true));
+
+    }
+
+    [HttpGet]
+    [Route("one/{title}")]
+    public async Task<IActionResult> GetNoticeByTitle(string title)
+    {
+        title = title.Replace("-", " ");
+
+        List<ArticleDTO> articles = await _ArticleService.GetAll();
+
+        var article = articles.FirstOrDefault(a => a.Title.ToLower() == title.ToLower());
+
+        if (article == null)
         {
-            _ArticleService = ArticleService;
+            return NotFound(Res.Provider(new { }, $"{title} no encontrado", false));
         }
+        return Ok(Res.Provider(article, $"{title} ha sido encontrado", true));
 
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
-        {
-            List<ArticleDTO> articles = await _ArticleService.GetAll();
-            return Ok(articles);
-        }
-
-        [HttpGet]
-        [Route("{type}")]
-
-        public async Task<IActionResult> GetNotices(string type)
-        {
-            List<ArticleDTO> articles = await _ArticleService.GetAll();
-            var filter = articles.Where(article => article.ArticleType == type).ToList();
-
-            var min = filter.Select(article => TinyMapper.Map<ArticleMinDTO>(article)).ToList();
-            return Ok(min);
-        }
-
-        [HttpGet]
-        [Route("{title}")]
-        public async Task<IActionResult> GetNoticeById(string title)
-        {
-            title = title.Replace("-", " ");
-
-            List<ArticleDTO> articles = await _ArticleService.GetAll();
-
-            var article = articles.FirstOrDefault(a => a.Title.ToLower() == title.ToLower());
-
-            if (article == null)
-            {
-                return NotFound();
-            }
-            return Ok(article);
-        }
+    }
 
 
-        [HttpPost]
-        public async Task<IActionResult> Insertar([FromBody] CreateArticle article)
-        {
-            Article newArticle = TinyMapper.Map<Article>(article);
+    [HttpPost]
+    public async Task<IActionResult> Insertar([FromBody] CreateArticle article)
+    {
+        Article newArticle = TinyMapper.Map<Article>(article);
 
 
-            bool result = await _ArticleService.Create(newArticle);
+        bool result = await _ArticleService.Create(newArticle);
+        if (result)
+            return Ok(Res.Provider(newArticle, "Agregado con exito", true));
 
-            return StatusCode(StatusCodes.Status200OK, new { success = result });
-        }
+        return Ok(Res.Provider(new { }, "No se pudo agregar", false));
 
-        [HttpDelete]
-        [Route("{Id:int}")]
-        public async Task<IActionResult> Delete(int Id)
-        {
-            bool result = await _ArticleService.Delete(Id);
+    }
 
-            return StatusCode(StatusCodes.Status200OK, new { success = result });
-        }
+    [HttpDelete]
+    [Route("{Id:int}")]
+    public async Task<IActionResult> Delete(int Id)
+    {
+        bool result = await _ArticleService.Delete(Id);
+
+        if (result)
+            return Ok(Res.Provider(new { }, "Eliminado con exito", true));
+
+        return Ok(Res.Provider(new { }, "No se pudo eliminar", false));
+    }
 }
