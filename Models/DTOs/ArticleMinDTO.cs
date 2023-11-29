ec28f89868380f85c10dcbe01a35c59cbf762316
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2023-11-28 21:24:47 -0600

    add system user in article min dto

--- Models/DTOs/ArticleMinDTO.cs
+++ Models/DTOs/ArticleMinDTO.cs
@@ -8,4 +8,6 @@ public class ArticleMinDTO
     public string? Keywords { get; set; }
     public string? CreateAt { get; set; }
     public string ArticleType { get; set; } = null!;
+    public SystemUserDTO SystemUser { get; set; } = null!;
+
 }
